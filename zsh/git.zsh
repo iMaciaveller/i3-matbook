a2e7ee9e0b3dd9e8263348bd0981a2d809597546
Maksim Totmin <maksim@totmin.ru>
2019-10-19 13:17:07 +0700

    add zsh

---
+++ zsh/git.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+# coding:     utf-8
+# Author:     Maksim Totmin, @iMaciaveller
+
+#git
+
+autoload -Uz vcs_info
+precmd_vcs_info() { vcs_info }
+precmd_functions+=( precmd_vcs_info )
+setopt prompt_subst
+zstyle ':vcs_info:git:*' formats '%b'
