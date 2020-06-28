86e3540f427613f0bc02813f72acd5720f7a9794
Maksim Totmin <maksim@totmin.ru>
2020-06-28 23:27:52 +0700

    fix git status

--- zsh/prompt.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-# coding:     utf-8
-# Author:     Maksim Totmin, @iMaciaveller
-
-#PROMPTS
-
-PROMPT=%{$fg_bold[blue]%}[%n@%m]'» '%{$reset_color%}
-#RPROMPT=%{$fg_bold[yellow]%}'[%~]'%{$reset_color%}%{$fg_bold[red]%}'[%T]'%{$reset_color%}
-RPROMPT=%{$fg_bold[green]%}[' '\$vcs_info_msg_0_]%{$reset_color%}%{$fg_bold[yellow]%}'[%~]'%{$reset_color%}%{$fg_bold[red]%}'[%T]'%{$reset_color%}
